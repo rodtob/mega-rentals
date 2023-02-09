994d4abd84a6a0c3a6c74dffc48691fd77238aeb
Rodrigo Toblli <rodrigotobli@TOBLI-Rodrigo-MacBook-Pro.local>
2023-02-09 23:40:20 +0100

    search component

---
+++ app/components/search-input/component.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+export default class SearchInputComponent extends Component {
+  @service router;
+
+  @tracked query = '';
+
+  @action
+  search() {
+    this.router.transitionTo('index');
+  }
+}
