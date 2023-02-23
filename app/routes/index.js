8582c2afc32f9ce98b35d146f0c0b0308e8b42da
Rodrigo Toblli <rodrigotobli@TOBLI-Rodrigo-MacBook-Pro.local>
2023-02-23 17:26:20 +0100

    dynamic search

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,7 +7,8 @@ export default class IndexRoute extends Route {
   @service store;
 
   async model(params) {
-    console.log(params);
+    // console.log(params);
+    // const qName = params.q || 'Holiday Inn';
     // const options = {
     //   method: 'GET',
     //   headers: {
@@ -17,12 +18,13 @@ export default class IndexRoute extends Route {
     // };
 
     // const response = await fetch(
-    //   'https://booking-com.p.rapidapi.com/v1/hotels/search?room_number=1&checkout_date=2023-08-19&dest_type=city&dest_id=-553173&adults_number=2&locale=en-gb&checkin_date=2023-08-18&order_by=popularity&filter_by_currency=AED&units=metric&page_number=0&children_number=2&include_adjacency=true&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_ages=5%2C0',
+    //   `https://booking-com.p.rapidapi.com/v1/hotels/locations?name=${qName}&locale=en-gb`,
     //   options
     // );
-    // const { result } = await response.json();
+    // const result = await response.json();
     // console.log(result);
-    //   return result;
+    // console.log(mockedData);
+    // return result;
     return mockedData;
   }
 }
